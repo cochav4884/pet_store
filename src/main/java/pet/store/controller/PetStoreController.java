1f6b8afafef17b3495a586cb6ed4684d55911126
[Corinne Padilla] <[corinnepadilla@yahoo.com]>
2024-10-31 12:12:28 -0700

    updating files2

--- src/main/java/pet/store/controller/PetStoreController.java
+++ src/main/java/pet/store/controller/PetStoreController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -27,10 +28,11 @@ public class PetStoreController {
 		return petStoreService.savePetStore(petStoreData);
 	}
 	
+	@PutMapping("/{petStoreId}")
 	public PetStoreData updatePetStore(@PathVariable Long petStoreId, @RequestBody PetStoreData petStoreData) {
 		petStoreData.setPetStoreId(petStoreId);
-		log.info("Updating pet store {}", petStoreService);
-		return petStoreService.savePetStore(petStoreService);
+		log.info("Updating pet store {}", petStoreData);
+		return petStoreService.savePetStore(petStoreData);
 	}
 
 }
--- src/main/java/pet/store/service/PetStoreService.java
+++ src/main/java/pet/store/service/PetStoreService.java
@@ -1,15 +1,40 @@
 package pet.store.service;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import pet.store.controller.model.PetStoreData;
+import pet.store.dao.PetStoreDao;
+import pet.store.entity.PetStore;
+
+import java.util.NoSuchElementException;
 
 @Service
 public class PetStoreService {
-    
-	public PetStoreData savePetStore(PetStoreService petStoreService) {
+    @Autowired
+    private PetStoreDao petStoreDao;
+
+    public PetStoreData savePetStore(PetStoreData petStoreData) {
+        PetStore petStore = findOrCreatePetStore(petStoreData.getPetStoreId());
+        copyPetStoreFields(petStore, petStoreData);
+        return new PetStoreData(petStoreDao.save(petStore));
+    }
+
+    private PetStore findOrCreatePetStore(Long petStoreId) {
+        if (petStoreId == null) {
+            return new PetStore(); // return a new PetStore instance
+        }
+        return petStoreDao.findById(petStoreId)
+                .orElseThrow(() -> new NoSuchElementException("No PetStore found with ID: " + petStoreId));
+    }
+
+    private void copyPetStoreFields(PetStore petStore, PetStoreData petStoreData) {
+        petStore.setPetStoreName(petStoreData.getPetStoreName());
+        petStore.setPetStoreAddress(petStoreData.getPetStoreAddress());
+        // Exclude customers and employees to avoid recursion
+    }
+
+	public PetStoreData savePetStore(PetStoreService petStoreData) {
 		return null;
-	}
+	}		
+	
+ }
 
-
-}
